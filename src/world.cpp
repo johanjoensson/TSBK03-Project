bf9498a7a5dc6a09ffbfbd4f0483af7dd5a2be32
Johan Jönsson <johjo939@student.liu.se>
2013-12-11 20:13:47 +0100

    Vi haz ground

--- src/world.cpp
+++ src/world.cpp
@@ -6,9 +6,9 @@ World::World()
   o = Body("src/obj/bunnyplus.obj", "src/obj/grass.tga");
   o.rotate('y', 3.14/2);
   cam = Camera();
-  ground = Body("src/obj/bunnyplus.obj", "src/obj/grass.tga");
-  ground.set_scale(0.5);
-  ground.update();
+  ground = Body("src/obj/untitled.obj", "src/obj/grass.tga");
+  ground.place(vec3(0,-1,0));
+  ground.scale(1000);
 }
 
 void World::draw(int program)
